2684698f7a3d21a6fc543e3c9a7bfcca02b22be4
raphael <moudens.raphael@orange.fr>
2017-03-13 16:57:59 +0100

    exercice en groupe

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
 //to do server
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+const urlJson = '/database/tasks.json';
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+// import routes from '/public/index.html';
+
+// app.use('/', routes);
+
+app.listen(2000, ()=>{
+	console.log('now on port 2000');
+});
--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,5 +1,24 @@
 (() => {
-  const app = {
-    //todo
-  }
+	const app = {
+		tasks:null,
+		init(){
+			this.getTasks();
+		},
+		getTasks(){
+			fetch('/database/tasks.json')
+			.then(res=>res.json())
+			.then(data=>{
+				this.tasks = data.tasks;
+				this.addTasks();
+			})
+		},
+		addTasks(){
+			this.tasks.map(task=>{
+				console.log(task.content)
+				document.getElementById('list').innerHTML += `<li>${task.content}</li>`
+			})
+
+		}
+	}
+	app.init();
 })();
